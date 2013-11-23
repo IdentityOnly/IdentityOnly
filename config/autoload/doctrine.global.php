de42ee353471df9c1470d53e86c9f360bbf82dd7
Justin Martin <frozenfire@thefrozenfire.com>
2013-11-23 13:50:43 -0800

    Added IdentityCommon module. Configured Doctrine's orm_default.

---
+++ config/autoload/doctrine.global.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'doctrine' => array(
+        'entitymanager' => array(
+            'orm_default' => array(
+                'connection' => 'identity',
+                'configuration' => 'identity',
+            )
+        )
+    )
+);
